89b8c5750a89c7e404314cc5590097f0c447155d
Damon-Nunez <damon.nunez@student.uncommonhs.org>
2021-11-08 15:04:35 +0000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //write your code here
+function ArrayAverage(arr){
+    var totalSum= 0;
+    for(let i=0; i<arr.length; i++){
+        totalSum += arr[i]
+    }
+    var arrlength= arr.length
+    var results = totalSum / arrlength
+    console.log(results);
+}
+ArrayAverage([5,3,4,5,7])
